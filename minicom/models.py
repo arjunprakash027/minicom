434678541b9415f696c072191ee3bb0f394e4a1f
Arjun Prakash <arjunprakash027@gmail.com>
2025-12-03 19:56:48 +0000

    making it simpler

--- minicom/models.py
+++ minicom/models.py
@@ -1,6 +1,5 @@
-mdbmfrom django.db import models
-from
- django.contrib.auth.models import User
+from django.db import models
+from django.contrib.auth.models import User
 
 
 class Message(models.Model):
